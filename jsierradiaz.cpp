04bb150676644a9698452335058153341618b4cf
Pablo <juansiediaz97@gmail.com>
2021-09-30 14:47:30 -0700

    personal funtionction

--- jsierradiaz.cpp
+++ jsierradiaz.cpp
@@ -1,1 +1,18 @@
 //Juan Sierra Diaz
+
+#include"fonts.h"
+
+void show_juan_credits(int x, int y){
+
+        Rect r;
+
+        r.bot = y;
+        r.left = x;
+        r.center = 0;
+
+        ggprint8b(&r, 16, 0x00A020F0, "Juan Sierra Diaz");
+
+
+
+}
+
