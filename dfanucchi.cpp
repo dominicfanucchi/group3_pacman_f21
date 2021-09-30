38f4a209527746bcc623007796502beff8a45125
Dominic Fanucchi <dfanucchi@odin.cs.csubak.edu>
2021-09-30 12:13:30 -0700

    added definitions

--- dfanucchi.cpp
+++ dfanucchi.cpp
@@ -11,6 +11,13 @@ using namespace std;
 
 int show_dominics_credits(int x, int y)
 {
-    
-   
+    Rect r;
+
+    r.bot = y;
+    r.left = x;
+    r.center = 1;
+
+    ggprint8b(&r, 16, 0x00ff0000, "Dominic Fanucchi");    
+
+    return 0;
 }
