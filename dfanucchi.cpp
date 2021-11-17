5c5b3f4b0106e730bac88add0f516a2e5f56b230
Dominic Fanucchi <dfanucchi@odin.cs.csubak.edu>
2021-11-17 11:00:45 -0800

    commented out code since it is not complete

--- dfanucchi.cpp
+++ dfanucchi.cpp
@@ -19,111 +19,111 @@ void show_dominics_credits(int x, int y)
     ggprint8b(&r, 16, 0x00ffff34, "Dominic Fanucchi");
 }
 
-void load_and_bind_textures()
-{
-	glEnable(GL_BLEND);
-    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+// void load_and_bind_textures()
+// {
+// 	glEnable(GL_BLEND);
+//     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
-	maze_tex = load_and_bind_texture("images/maze.png");
-}
+// 	maze_tex = load_and_bind_texture("images/maze.png");
+// }
 
-unsigned int load_and_bind_texture(const char* filename)
-{
+// unsigned int load_and_bind_texture(const char* filename)
+// {
 	
-}
+// }
 
-typedef enum {#, g, u} tile;
-// Tile defined as an enum where each type is defined as:
-// #: Wall
-// g: Ghost Pen Gate
-// u: Empty path
-tile maze[28][31] =
-{
-    {#,#,#,#,#,#,#,#,#,#,#,#,u,u,u,#,u,#,u,u,u,#,#,#,#,#,#,#,#,#,#},
-    {#,u,u,u,u,#,#,u,u,u,u,#,u,u,u,#,u,#,u,u,u,#,u,u,u,u,u,u,u,u,#},
-    {#,u,#,#,u,#,#,u,#,#,u,#,u,u,u,#,u,#,u,u,u,#,u,#,#,u,#,#,#,u,#},
-    {#,u,#,#,u,u,u,u,#,#,u,#,u,u,u,#,u,#,u,u,u,#,u,#,#,u,#,#,#,u,#},
-    {#,u,#,#,u,#,#,#,#,#,u,#,u,u,u,#,u,#,u,u,u,#,u,#,#,u,#,#,#,u,#},
-    {#,u,#,#,u,#,#,#,#,#,u,#,#,#,#,#,u,#,#,#,#,#,u,#,#,u,#,#,#,u,#},
-    {#,u,#,#,u,u,u,u,u,u,u,u,u,u,u,u,u,u,u,u,u,u,u,u,u,u,u,u,u,u,#},
-    {#,u,#,#,#,#,#,u,#,#,u,#,#,#,#,#,u,#,#,#,#,#,#,#,#,u,#,#,#,u,#},
-    {#,u,#,#,#,#,#,u,#,#,u,#,#,#,#,#,u,#,#,#,#,#,#,#,#,u,#,#,#,u,#},
-    {#,u,#,#,u,u,u,u,#,#,u,u,u,u,u,u,u,u,u,u,#,#,u,u,u,u,#,#,#,u,#},
-    {#,u,#,#,u,#,#,u,#,#,u,#,#,u,#,#,#,#,#,u,#,#,u,#,#,u,#,#,#,u,#},
-    {#,u,#,#,u,#,#,u,#,#,u,#,#,u,#,u,u,u,#,u,#,#,u,#,#,u,#,#,#,u,#},
-    {#,u,u,u,u,#,#,u,u,u,u,#,#,u,#,u,u,u,#,u,u,u,u,#,#,u,u,u,u,u,#},
-    {#,u,#,#,#,#,#,u,#,#,#,#,#,u,#,u,u,u,g,u,#,#,#,#,#,u,#,#,#,#,#},
-    {#,u,#,#,#,#,#,u,#,#,#,#,#,u,#,u,u,u,g,u,#,#,#,#,#,u,#,#,#,#,#},
-    {#,u,u,u,u,#,#,u,u,u,u,#,#,u,#,u,u,u,#,u,u,u,u,#,#,u,u,u,u,u,#},
-    {#,u,#,#,u,#,#,u,#,#,u,#,#,u,#,u,u,u,#,u,#,#,u,#,#,u,#,#,#,u,#},
-    {#,u,#,#,u,#,#,u,#,#,u,#,#,u,#,#,#,#,#,u,#,#,u,#,#,u,#,#,#,u,#},
-    {#,u,#,#,u,u,u,u,#,#,u,u,u,u,u,u,u,u,u,u,#,#,u,u,u,u,#,#,#,u,#},
-    {#,u,#,#,#,#,#,u,#,#,u,#,#,#,#,#,u,#,#,#,#,#,#,#,#,u,#,#,#,u,#},
-    {#,u,#,#,#,#,#,u,#,#,u,#,#,#,#,#,u,#,#,#,#,#,#,#,#,u,#,#,#,u,#},
-    {#,u,#,#,u,u,u,u,u,u,u,u,u,u,u,u,u,u,u,u,u,u,u,u,u,u,u,u,u,u,#},
-    {#,u,#,#,u,#,#,#,#,#,u,#,#,#,#,#,u,#,#,#,#,#,u,#,#,u,#,#,#,u,#},
-    {#,u,#,#,u,#,#,#,#,#,u,#,u,u,u,#,u,#,u,u,u,#,u,#,#,u,#,#,#,u,#},
-    {#,u,#,#,u,u,u,u,#,#,u,#,u,u,u,#,u,#,u,u,u,#,u,#,#,u,#,#,#,u,#},
-    {#,u,#,#,u,#,#,u,#,#,u,#,u,u,u,#,u,#,u,u,u,#,u,#,#,u,#,#,#,u,#},
-    {#,u,u,u,u,#,#,u,u,u,u,#,u,u,u,#,u,#,u,u,u,#,u,u,u,u,u,u,u,u,#},
-    {#,#,#,#,#,#,#,#,#,#,#,#,u,u,u,#,u,#,u,u,u,#,#,#,#,#,#,#,#,#,#}
-};
+// typedef enum {#, g, u} tile;
+// // Tile defined as an enum where each type is defined as:
+// // #: Wall
+// // g: Ghost Pen Gate
+// // u: Empty path
+// tile maze[28][31] =
+// {
+//     {#,#,#,#,#,#,#,#,#,#,#,#,u,u,u,#,u,#,u,u,u,#,#,#,#,#,#,#,#,#,#},
+//     {#,u,u,u,u,#,#,u,u,u,u,#,u,u,u,#,u,#,u,u,u,#,u,u,u,u,u,u,u,u,#},
+//     {#,u,#,#,u,#,#,u,#,#,u,#,u,u,u,#,u,#,u,u,u,#,u,#,#,u,#,#,#,u,#},
+//     {#,u,#,#,u,u,u,u,#,#,u,#,u,u,u,#,u,#,u,u,u,#,u,#,#,u,#,#,#,u,#},
+//     {#,u,#,#,u,#,#,#,#,#,u,#,u,u,u,#,u,#,u,u,u,#,u,#,#,u,#,#,#,u,#},
+//     {#,u,#,#,u,#,#,#,#,#,u,#,#,#,#,#,u,#,#,#,#,#,u,#,#,u,#,#,#,u,#},
+//     {#,u,#,#,u,u,u,u,u,u,u,u,u,u,u,u,u,u,u,u,u,u,u,u,u,u,u,u,u,u,#},
+//     {#,u,#,#,#,#,#,u,#,#,u,#,#,#,#,#,u,#,#,#,#,#,#,#,#,u,#,#,#,u,#},
+//     {#,u,#,#,#,#,#,u,#,#,u,#,#,#,#,#,u,#,#,#,#,#,#,#,#,u,#,#,#,u,#},
+//     {#,u,#,#,u,u,u,u,#,#,u,u,u,u,u,u,u,u,u,u,#,#,u,u,u,u,#,#,#,u,#},
+//     {#,u,#,#,u,#,#,u,#,#,u,#,#,u,#,#,#,#,#,u,#,#,u,#,#,u,#,#,#,u,#},
+//     {#,u,#,#,u,#,#,u,#,#,u,#,#,u,#,u,u,u,#,u,#,#,u,#,#,u,#,#,#,u,#},
+//     {#,u,u,u,u,#,#,u,u,u,u,#,#,u,#,u,u,u,#,u,u,u,u,#,#,u,u,u,u,u,#},
+//     {#,u,#,#,#,#,#,u,#,#,#,#,#,u,#,u,u,u,g,u,#,#,#,#,#,u,#,#,#,#,#},
+//     {#,u,#,#,#,#,#,u,#,#,#,#,#,u,#,u,u,u,g,u,#,#,#,#,#,u,#,#,#,#,#},
+//     {#,u,u,u,u,#,#,u,u,u,u,#,#,u,#,u,u,u,#,u,u,u,u,#,#,u,u,u,u,u,#},
+//     {#,u,#,#,u,#,#,u,#,#,u,#,#,u,#,u,u,u,#,u,#,#,u,#,#,u,#,#,#,u,#},
+//     {#,u,#,#,u,#,#,u,#,#,u,#,#,u,#,#,#,#,#,u,#,#,u,#,#,u,#,#,#,u,#},
+//     {#,u,#,#,u,u,u,u,#,#,u,u,u,u,u,u,u,u,u,u,#,#,u,u,u,u,#,#,#,u,#},
+//     {#,u,#,#,#,#,#,u,#,#,u,#,#,#,#,#,u,#,#,#,#,#,#,#,#,u,#,#,#,u,#},
+//     {#,u,#,#,#,#,#,u,#,#,u,#,#,#,#,#,u,#,#,#,#,#,#,#,#,u,#,#,#,u,#},
+//     {#,u,#,#,u,u,u,u,u,u,u,u,u,u,u,u,u,u,u,u,u,u,u,u,u,u,u,u,u,u,#},
+//     {#,u,#,#,u,#,#,#,#,#,u,#,#,#,#,#,u,#,#,#,#,#,u,#,#,u,#,#,#,u,#},
+//     {#,u,#,#,u,#,#,#,#,#,u,#,u,u,u,#,u,#,u,u,u,#,u,#,#,u,#,#,#,u,#},
+//     {#,u,#,#,u,u,u,u,#,#,u,#,u,u,u,#,u,#,u,u,u,#,u,#,#,u,#,#,#,u,#},
+//     {#,u,#,#,u,#,#,u,#,#,u,#,u,u,u,#,u,#,u,u,u,#,u,#,#,u,#,#,#,u,#},
+//     {#,u,u,u,u,#,#,u,u,u,u,#,u,u,u,#,u,#,u,u,u,#,u,u,u,u,u,u,u,u,#},
+//     {#,#,#,#,#,#,#,#,#,#,#,#,u,u,u,#,u,#,u,u,u,#,#,#,#,#,#,#,#,#,#}
+// };
 
-void translate_to_maze_coords(float x, float y)
-{
-    glTranslatef(x * 8, y * 8, 0.0f);
-}
+// void translate_to_maze_coords(float x, float y)
+// {
+//     glTranslatef(x * 8, y * 8, 0.0f);
+// }
 
-void translate_bottom_left()
-{
-    glTranslatef(38.0f, 26.0f, 0.0f);
-}
+// void translate_bottom_left()
+// {
+//     glTranslatef(38.0f, 26.0f, 0.0f);
+// }
 
-tile get_tile(float x, float y)
-{
-    return maze[(int) floor(x)][(int) floor(y)];
-}
+// tile get_tile(float x, float y)
+// {
+//     return maze[(int) floor(x)][(int) floor(y)];
+// }
 
-void drawMaze()
-{
-    glPushMatrix();
-    translate_bottom_left();              // Translate to bottom left of screen
-    draw_texture(maze_tex, 224, 248, 0);     // Draw the map walls using the sprite
-    glPopMatrix();
-}
+// void drawMaze()
+// {
+//     glPushMatrix();
+//     translate_bottom_left();              // Translate to bottom left of screen
+//     draw_texture(maze_tex, 224, 248, 0);     // Draw the map walls using the sprite
+//     glPopMatrix();
+// }
 
-void draw_texture(unsigned int texture, int length, int height, float angle)
-{
-    // Begin new transformation matrix
-    glPushMatrix();
-    glColor3f(1.0f, 1.0f, 1.0f);
+// void draw_texture(unsigned int texture, int length, int height, float angle)
+// {
+//     // Begin new transformation matrix
+//     glPushMatrix();
+//     glColor3f(1.0f, 1.0f, 1.0f);
 
-    int widthCenter = length/2;
-    int verticalCenter = height/2;
+//     int widthCenter = length/2;
+//     int verticalCenter = height/2;
 
-    // Translate to center of sprite to rotate about its origin (for sprites such as pacman)
-    glTranslatef((float)widthCenter,(float)verticalCenter,0.0f);
-    glRotatef(angle, 0.0f, 0.0f, 1.0f);
+//     // Translate to center of sprite to rotate about its origin (for sprites such as pacman)
+//     glTranslatef((float)widthCenter,(float)verticalCenter,0.0f);
+//     glRotatef(angle, 0.0f, 0.0f, 1.0f);
 
-    // Enable texturing and bind the specific sprite
-    glEnable(GL_TEXTURE_2D);
-    glBindTexture(GL_TEXTURE_2D, texture);
+//     // Enable texturing and bind the specific sprite
+//     glEnable(GL_TEXTURE_2D);
+//     glBindTexture(GL_TEXTURE_2D, texture);
 
-    // texture coordinates
-    glBegin(GL_QUADS);
-        glTexCoord2f (0.0f, 0.0f);      // Lower left corner
-        glTexCoord2f (1.0f, 0.0f);      // Lower right corner
-        glTexCoord2f (1.0f, 1.0f);      // Upper right corner
-        glTexCoord2f (0.0f, 1.0f);      // Upper left corner
+//     // texture coordinates
+//     glBegin(GL_QUADS);
+//         glTexCoord2f (0.0f, 0.0f);      // Lower left corner
+//         glTexCoord2f (1.0f, 0.0f);      // Lower right corner
+//         glTexCoord2f (1.0f, 1.0f);      // Upper right corner
+//         glTexCoord2f (0.0f, 1.0f);      // Upper left corner
 
-        glVertex2i(-widthCenter, -verticalCenter);
-        glVertex2i( widthCenter, -verticalCenter);
-        glVertex2i( widthCenter,  verticalCenter);
-        glVertex2i(-widthCenter,  verticalCenter);
-    glEnd();
+//         glVertex2i(-widthCenter, -verticalCenter);
+//         glVertex2i( widthCenter, -verticalCenter);
+//         glVertex2i( widthCenter,  verticalCenter);
+//         glVertex2i(-widthCenter,  verticalCenter);
+//     glEnd();
 
-    glDisable(GL_TEXTURE_2D);
+//     glDisable(GL_TEXTURE_2D);
 
-    // Pop matrix to ignore above transformations on future objects
-    glPopMatrix();
-}
+//     // Pop matrix to ignore above transformations on future objects
+//     glPopMatrix();
+// }
