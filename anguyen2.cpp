ff6a4fccd52af4c73e1af3dec10d2cfef3dec7cb
MrSmileyFac3 <89361721+MrSmileyFac3@users.noreply.github.com>
2021-11-30 19:42:25 -0800

    update anguyen2.cpp

--- anguyen2.cpp
+++ anguyen2.cpp
@@ -1,17 +1,19 @@
-/****************************************************************
-*   CMPS-3350                                                   *
-*   Creator: Andrew Nguyen                                      *
-*   File name: anguyen2.cpp                                     *
-*   Objective: Desgin Scoring, Dashboard, pellets, and ghosts   *
-*   Project due Nov 22                                          *
-*****************************************************************/
+
+/************************************************************************
+*   CMPS-3350                                                           *
+*   Creator: Andrew Nguyen                                              *
+*   File name: anguyen2.cpp                                             *
+*   Objective: Desgin Scoring, Dashboard, pellets, ghost, pacman        *
+*   Project prestation Dec 2                                            *
+************************************************************************/
 
 #include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
-using namespace std;
 #include <string>
 #include <math.h>
+
+using namespace std;
 
 #include "fonts.h"
 void show_andrew_credits(int x, int y)
@@ -23,13 +25,8 @@ void show_andrew_credits(int x, int y)
     ggprint8b(&r, 16, 0x00ff0000, "Andrew Nguyen");
 }
 
-const int TILE_WIDTH  = 30;
-const int TILE_HEIGHT = 30;
-int coin; 
-
 #ifndef pellet
 #define pellet
-
 class Pellet
 {
 public:
@@ -39,11 +36,42 @@ public:
     //render pellet
     void render();
     
-    //destroy pellet (when boxman eats it)
+    //destroy pellet (when pacman eats it)
     void destroy();
 };
 #endif
 
+// ERROR: expected expression 
+//Constructor 
+
+Pellet::Pellet(int pellet_type, int x, int y)
+{
+    //regular pellet
+    //if (pellet_type == 1)
+        //coin =  {(x + TILE_WIDTH/2 - 2), (y + TILE_HEIGHT/2 - 2), 4, 4};
+    //big pellet
+    //else if (pellet_type == 2)
+        //coin = {(x + TILE_WIDTH/2 - 6), (y + TILE_HEIGHT/2 - 6), 14, 14};
+    //if no pellet
+    //else if (pellet_type == 0)
+        //coin = {0, 0, 0, 0};
+}
+/*
+//render pellet
+void Pellet::render()
+{
+    Render( &coin);
+}
+
+//remove pellet
+void Pellet::destroy()
+{
+    coin = {0, 0, 0, 0 };
+}
+*/
+
+#ifndef collider
+#define collider
 class Collider
 {
 public:
@@ -53,40 +81,75 @@ public:
     //coordinate accessors
     int get_x();
     int get_y();
+
     //dimension accessors
     int get_width();
     int get_height();
+
+    bool collided(Collider object2);
+
+    protected:
+    //used to check for collisions
+    int Coin;
     
 };
+#endif 
 
-// ERROR: expected expression 
-//Constructor 
-
-Pellet::Pellet(int pellet_type, int x, int y)
-{
-    //if regular pellet
-    //if (pellet_type == 1)
-        //coin =  {(x + TILE_WIDTH/2 - 2), (y + TILE_HEIGHT/2 - 2), 4, 4};
-    //if big pellet
-    //else if (pellet_type == 2)
-        //coin = {(x + TILE_WIDTH/2 - 6), (y + TILE_HEIGHT/2 - 6), 14, 14};
-    //if no pellet
-    //else if (pellet_type == 0)
-        //coin = {0, 0, 0, 0};
-}
 /*
-//render pellet
-void render()
+Collider::Collider()
 {
-    Render( &coin);
+    Coin = { 0, 0, 0, 0 };
 }
 
-//remove pellet
-void destroy()
+int Collider::get_x()
 {
-    coin = {0, 0, 0, 0 };
+    return Coin.x;
+}
+
+int Collider::get_y()
+{
+    return Coin.y;
+}
+
+int Collider::get_width()
+{
+    return Coin.w;
+}
+
+int Collider::get_height()
+{
+    return Coin.h;
 }
 */
+
+//linker command failed 
+
+//check to see if objects collided
+/*bool Collider::collided(Collider object2)
+{
+    //if calling object is to the right of object 2. Cannot collided 
+    if ( get_x() >= object2.get_x() + object2.get_width() )
+        return false;
+    
+    //if calling object is to the left of object 2. Cannot collided 
+    if ( get_x() <= object2.get_x() - get_width() )
+        return false;
+    
+    //if calling object is below object 2. Cannot collided 
+    if ( get_y() >= object2.get_y() + object2.get_height() )
+        return false;
+    
+    //if calling object is above object 2. Cannot collided 
+    if ( get_y() <= object2.get_y() - get_height() )
+        return false;
+    
+    //otherwise, it must have collided with pacman or ghost 
+    return true;
+}*/
+
+const int TILE_WIDTH  = 30;
+const int TILE_HEIGHT = 30;
+int coin; 
 
 //Dashboard 
 class Dashboard
@@ -168,13 +231,12 @@ void ghost::changePosition(int changeX, int changeY)
     y = y + changeY;
 }
 
-/*
-int main()
+/*int main()
 {
     char choice; 
 
     cout<<"MENU: "<<endl;
-    /*cout<<"1 : CREATE ACCOUNT "<<endl;
+    cout<<"1 : CREATE ACCOUNT "<<endl;
     cout<<"2 : LOGIN"<<endl;
     cout<<"3 : DELETE ACCOUNT"<<endl;
     cout<<"4 : VIEW ACCOUNT DETAILS"<<endl;
@@ -187,5 +249,4 @@ int main()
     cin>>choice;
     
     return 0; 
-}
-*/ 
+}*/
